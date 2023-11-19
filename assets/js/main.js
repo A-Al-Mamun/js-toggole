fc63872bab31e6c33b87adb0d5c878620cce40f6
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-19 15:39:56 +0600

    Nav menu toggole working practice

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+
+let barBtn = document.getElementsByClassName('btnBars')[0];
+
+
+barBtn.addEventListener('click', () => {
+    document.getElementsByClassName('menu')[0].classList.toggle('active');
+})
